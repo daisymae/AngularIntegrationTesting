8d12f3509e5f06c20370992c4363fa6778e3d576
daisymae <daisymae.orcadiafarm@gmail.com>
2019-01-21 13:23:33 -0600

    Update todos.component.spec.ts
    Add dependencies needed to compile and run.

--- src/app/2-todos/todos.component.spec.ts
+++ src/app/2-todos/todos.component.spec.ts
@@ -2,28 +2,40 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { By } from '@angular/platform-browser';
 import { DebugElement } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
 
 import { TodosComponent } from './todos.component';
+import { TodoService } from './todo.service';
 
 //NOTE: I've deliberately excluded this suite from running
 // because the test will fail. This is because we have not 
 // provided the TodoService as a dependency to TodosComponent. 
 // 
 // When you get to Lecture 6 (Providing Dependencies), be sure
-// to remove "x" from "xdescribe" below. 
+// to remove "x" from "xdescribe" below.
 
-xdescribe('TodosComponent', () => {
+// Not enough to provide just TodoServie in configureTestingModule, as it
+// depends on HttpClient, and will get this error:
+//     NullInjectorError: No provider for HttpClient!
+// So need to import HttpClient as well
+
+describe('TodosComponent', () => {
   let component: TodosComponent;
   let fixture: ComponentFixture<TodosComponent>;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ TodosComponent ]
+      imports: [ HttpClientModule ],
+      declarations: [ TodosComponent ],
+      providers: [ TodoService ]
     })
     .compileComponents();
   }));
 
   beforeEach(() => {
+    // in unit tests do it this way:
+    // new TodosComponent(new TodoService()); // and use a spyOn for the service;
+    // this doesn't work for integration; instead register as provider in testingModule
     fixture = TestBed.createComponent(TodosComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
