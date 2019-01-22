61653327f724456d3f4706c66dfd5bc58809e590
daisymae <daisymae.orcadiafarm@gmail.com>
2019-01-21 20:28:07 -0600

    Route tests
    Added tests for the route using stubs for testing in the component. Added tests for the route at the app level.

--- src/app/3-user-details/user-details.component.spec.ts
+++ src/app/3-user-details/user-details.component.spec.ts
@@ -3,15 +3,30 @@ import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { By } from '@angular/platform-browser';
 import { DebugElement } from '@angular/core';
 
-import { UserDetailsComponent } from './user-details.component';
+class RouterStub {
+  navigate(params) {
+  }
+}
 
-xdescribe('UserDetailsComponent', () => {
+class ActivatedRouteStub {
+  params: Observable<any> = empty();
+}
+
+import { UserDetailsComponent } from './user-details.component';
+import { Router, ActivatedRoute } from '@angular/router';
+import { Observable, empty } from 'rxjs';
+
+describe('UserDetailsComponent', () => {
   let component: UserDetailsComponent;
   let fixture: ComponentFixture<UserDetailsComponent>;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ UserDetailsComponent ]
+      declarations: [ UserDetailsComponent ],
+      providers: [
+        { provide: Router, useClass: RouterStub }, // provide stub instead
+        { provide: ActivatedRoute, useClass: ActivatedRouteStub } // provide stub instead
+      ]
     })
     .compileComponents();
   }));
@@ -25,4 +40,14 @@ xdescribe('UserDetailsComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  // test interaction with router
+  it('should redirect the user to the users page after saving', () => {
+    let router = TestBed.get(Router);
+    let spy = spyOn(router, 'navigate');
+
+    component.save();
+
+    expect(spy).toHaveBeenCalledWith(['users']);
+  })
 });
