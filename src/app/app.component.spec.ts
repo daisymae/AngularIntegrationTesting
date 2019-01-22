a14f77816c0e1c29759241ccc364c699b1ec9903
daisymae <daisymae.orcadiafarm@gmail.com>
2019-01-22 07:03:38 -0600

    Refactored nav
    Refactored nav to be a component.
    Moved nav code to new nav component. Moved tests for nav to the new nav component.
    Modified route tests to handle new nav component usage (child component).

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -2,10 +2,11 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing'; 
 import { By } from '@angular/platform-browser';
-import { DebugElement } from '@angular/core';
+import { DebugElement, NO_ERRORS_SCHEMA } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { RouterOutlet, RouterLinkWithHref } from '@angular/router';
+import { NavComponent } from './nav/nav.component';
 
 describe('AppComponent', () => {
   let component: AppComponent;
@@ -15,7 +16,11 @@ describe('AppComponent', () => {
     TestBed.configureTestingModule({
       // RouterTestingModule gives us special testing access to RouterModule
       imports: [ RouterTestingModule.withRoutes([]) ], 
-      declarations: [ AppComponent ]
+      declarations: [ AppComponent ],
+      schemas: [ NO_ERRORS_SCHEMA ] // tells Angular to ignore any elements or attributes it doesn't recognize;
+      // alternative is to add NavComponent in declarations (for this test so far)
+      // this may be easier if have a component with a lot of child components
+      // potential problem: tests will not catch typo issues
     })
     .compileComponents();
   }));
@@ -32,13 +37,6 @@ describe('AppComponent', () => {
     expect(de).not.toBeNull();
   });
 
-  it('should have a link to todos page', () => {
-    let debugElements = fixture.debugElement.queryAll(By.directive(RouterLinkWithHref));
-
-    // note that even though the routerLink value is 'todos', the href expected here is '/todos'
-    let index = debugElements.findIndex(de => de.properties['href'] === '/todos');
-    expect(index).toBeGreaterThan(-1);
-  });
 
 
 });
