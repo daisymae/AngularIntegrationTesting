e438842278c633e4c1615d43b17a95ae052a53e0
daisymae <daisymae.orcadiafarm@gmail.com>
2019-01-22 06:50:42 -0600

    Update app.component.spec.ts
    Tests for RouterOutlet

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -5,6 +5,7 @@ import { By } from '@angular/platform-browser';
 import { DebugElement } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { RouterOutlet, RouterLinkWithHref } from '@angular/router';
 
 describe('AppComponent', () => {
   let component: AppComponent;
@@ -12,6 +13,8 @@ describe('AppComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
+      // RouterTestingModule gives us special testing access to RouterModule
+      imports: [ RouterTestingModule.withRoutes([]) ], 
       declarations: [ AppComponent ]
     })
     .compileComponents();
@@ -22,4 +25,20 @@ describe('AppComponent', () => {
     component = fixture.componentInstance;
     fixture.detectChanges(); 
   });
+
+  it('should have a router outlet', () => {
+    let de = fixture.debugElement.query(By.directive(RouterOutlet));
+
+    expect(de).not.toBeNull();
+  });
+
+  it('should have a link to todos page', () => {
+    let debugElements = fixture.debugElement.queryAll(By.directive(RouterLinkWithHref));
+
+    // note that even though the routerLink value is 'todos', the href expected here is '/todos'
+    let index = debugElements.findIndex(de => de.properties['href'] === '/todos');
+    expect(index).toBeGreaterThan(-1);
+  });
+
+
 });
